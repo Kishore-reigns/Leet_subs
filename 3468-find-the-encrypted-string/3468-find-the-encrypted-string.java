91120cb2025c0a4666096ccacc04789804cb901d
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2024-09-07 13:18:35 +0530

    Time: 1 ms (100%), Space: 42.7 MB (43.33%) - LeetHub

--- 3468-find-the-encrypted-string/3468-find-the-encrypted-string.java
+++ 3468-find-the-encrypted-string/3468-find-the-encrypted-string.java
@@ -1,10 +1,10 @@
 class Solution {
     public String getEncryptedString(String s, int k) {
         int n = s.length() ; 
-        String enc = new String() ; 
+        StringBuilder enc = new StringBuilder() ; 
         for(int i = 0 ; i < s.length() ; i++){
-            enc += s.charAt((i+k)%n) ; 
+            enc.append(s.charAt((i+k)%n))  ; 
         }
-        return enc ; 
+        return enc.toString() ; 
     }
 }
