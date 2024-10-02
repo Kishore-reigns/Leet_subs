bfa3111f478ca8372f3b4f626e03fff7349b7f7c
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2024-10-02 11:26:32 +0530

    Time: 26 ms (69.92%), Space: 62.4 MB (29.84%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -2,15 +2,14 @@ class Solution {
     public int[] arrayRankTransform(int[] arr) {
         int n = arr.length ; 
         int[]rank = new int[n];
-         PriorityQueue<Integer>queue = new PriorityQueue<>() ; 
-         for(int x : arr)queue.offer(x);
+        int[]sorted = arr.clone();
+        Arrays.sort(sorted);
          HashMap<Integer,Integer>map = new HashMap<>();
          int r = 1 ; 
-         while(!queue.isEmpty()){
-            int x = queue.poll() ; 
-            if(!map.containsKey(x)){
-                map.put(x,r);
-                r++ ; 
+         for(int i = 0 ; i < n ; i++){
+            if(!map.containsKey(sorted[i])){
+                map.put(sorted[i],r++);
+               
             }
          }
          for(int i = 0 ; i < n ;i++){
