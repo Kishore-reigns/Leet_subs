42ca2c453597fc7fa0fcc587deaabc3f69272b51
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2024-12-12 07:59:57 +0530

    Time: 8 ms (68.73%), Space: 45 MB (23.33%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,25 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length ; 
-        int[][] dp = new int[n][2] ;
-        return helper(0,nums,n,0,target);
+        int sum = 0 ; 
+        for(int v : nums)sum+= v ; 
+        int[][] dp = new int[n][2*sum+1] ;
+        for(int i = 0 ; i < n ; i++)Arrays.fill(dp[i],-1);
+        return helper(0,nums,n,0,target,dp,sum);
     }
 
-    public int helper(int i , int[] nums ,int n, int sum , int target){
+    public int helper(int i , int[] nums ,int n, int sum , int target,int dp[][],int tot){
 
         if(i == n){
             if(sum == target)return 1 ;
             return 0 ; 
         }
 
-        int plus = helper(i+1,nums,n,nums[i]+sum,target);
-        int diff = helper(i+1,nums,n,0-nums[i]+sum,target);
+        if(dp[i][sum+ tot]!= -1)return dp[i][sum + tot] ; 
 
-        return plus+diff ; 
+        int plus = helper(i+1,nums,n,nums[i]+sum,target,dp,tot);
+        int diff = helper(i+1,nums,n,0-nums[i]+sum,target,dp,tot);
+
+        return dp[i][sum+tot] = plus+diff ; 
     }
 }
