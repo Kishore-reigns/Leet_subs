5a3f710ab2c3ad5f840c1f7f273cedda9ce5dc20
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-01-01 11:33:29 +0530

    Time: 5 ms (20.99%), Space: 41.7 MB (36.08%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxScore(String s) {
+        int n = s.length();
+        int[] one = new int[n+1] ; 
+        Arrays.fill(one,0);
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(s.charAt(i) == '1'){
+                one[i] = one[i+1] + 1;
+            }else{
+                one[i] = one[i+1];
+            }
+            
+        }
+        //if(one[0] == s.length())return one[0]-1 ; 
+        int zero = 0 , res = -1 ; 
+        System.out.println(Arrays.toString(one));
+        for(int i = 0 ; i < n-1 ; i++){
+            if(s.charAt(i) == '0')zero++ ; 
+            res = Math.max(res,zero+one[i+1]);
+        }
+        return res ; 
+    }
+}
