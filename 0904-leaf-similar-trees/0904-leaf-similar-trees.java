0ac29fd6442c038710e70b6e66e4e83cefbbc8ef
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-01-05 20:28:15 +0530

    Time: 0 ms (100%), Space: 41.8 MB (17.01%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.java
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.java
@@ -18,10 +18,9 @@ class Solution {
         List<Integer>list = new ArrayList<>();
          List<Integer>list2 = new ArrayList<>();
         traverse(root1,list);
-        System.out.println(list);
         traverse(root2,list2);
-        System.out.println(list2);
-        return list.toString().equals(list2.toString())  ; 
+     
+        return list.equals(list2)  ; 
     }
     public void traverse(TreeNode root , List<Integer>list){
         if(root == null)return ; 
@@ -31,12 +30,5 @@ class Solution {
         traverse(root.right,list);
     }
 
-    public void traverse2(TreeNode root , List<Integer>list){
-        if(root == null)return ; 
-
-        if(root.left == null && root.right == null && list.get(0) == root.val)list.remove(0);
-
-        traverse2(root.left,list);
-        traverse(root.right,list);
-    }
+   
 }
