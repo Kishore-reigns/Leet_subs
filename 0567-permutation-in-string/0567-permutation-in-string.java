95cb3b3608a577652d5862e4f14666420d62626d
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-06-09 21:35:27 +0530

    Time: 5 ms (96.36%), Space: 42.5 MB (88.83%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,33 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length() ; 
-
-        if(s1.length() > s2.length() )return false ; 
-
-        int[] s1count = new int[26] ; 
-        int[] s2count = new int[26] ;
-
-        for(int i = 0 ; i < n ; i++){
-            s1count[ s1.charAt(i) - 'a']++ ; 
-            s2count[ s2.charAt(i) - 'a']++ ; 
+        int n1 = s1.length() , n2 = s2.length() ; 
+        if(n2 < n1)return false ; 
+        int[]f1 = new int[26];
+        int[]f2 = new int[26]; 
+        for(int i = 0 ; i < n1 ; i++){
+            f1[s1.charAt(i)-'a']++ ;
+            f2[s2.charAt(i)-'a']++ ; 
         }
-
-        if(matches(s1count,s2count))return true ; 
-
-        for(int i = n ; i < s2.length() ; i++ ){
-            s2count[s2.charAt(i-n) - 'a']-- ; 
-            s2count[ s2.charAt(i) - 'a']++ ;
-            if(matches(s1count,s2count))return true ;  
+        int l = 0 , r = n1 ; 
+      
+        if(Arrays.equals(f1,f2))return true ; 
+        while(l < n2-n1){
+            f2[s2.charAt(l)-'a']-- ; 
+            f2[s2.charAt(r)-'a']++ ;
+            l++ ; r++ ; 
+            if(Arrays.equals(f1,f2))return true ; 
         }
-
         return false ; 
-       
-    }
 
-    public boolean matches(int[] s1 , int[] s2){
-        for(int i = 0 ; i < 26 ; i++){
-            if(s1[i] != s2[i])return false ; 
-        }
-        return true ; 
+
     }
 }
