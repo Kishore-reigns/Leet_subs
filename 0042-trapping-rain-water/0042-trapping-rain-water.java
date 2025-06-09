db86d935e72b4e7de43fb4b6b3254f15a84671f0
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-06-09 20:53:36 +0530

    Time: 1 ms (62.38%), Space: 46.1 MB (81.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,20 +1,18 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length , total = 0 ;
-        int[] premax = new int[height.length];
-        int[] sufmax = new int[height.length];
-        premax[0] = height[0] ; 
-        for(int i = 1 ; i < n ; i++){
-            premax[i] = Math.max(height[i],premax[i-1]);
-        }
-        sufmax[n-1] = height[n-1] ; 
-        for(int i = n-2 ; i >= 0 ; i--){
-            sufmax[i] = Math.max(height[i],sufmax[i+1]);
-        }
-        for(int i = 0 ;i < n ; i++){
-            total += Math.min(premax[i],sufmax[i]) - height[i] ; 
-        }
+        int n = height.length , res = 0 ;
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        pre[0] = height[0];
+       
+        suf[n-1] = height[n-1];
+        for(int i = 1 ; i < n ; i++)pre[i] = Math.max(pre[i-1],height[i]);
+        for(int i = n-2 ; i>-1;i--)suf[i] = Math.max(suf[i+1],height[i]);
 
-        return total ; 
+        for(int i = 0 ; i < n ; i++){
+            res += Math.abs(Math.min(pre[i],suf[i]) - height[i]);
+        }
+        return res ; 
+
     }
 }
