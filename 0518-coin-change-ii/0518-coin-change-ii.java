0beeb39f11c55662ff92edc15de986ae5013a261
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-07-08 05:03:00 +0530

    Time: 20 ms (17.86%), Space: 49.3 MB (36.96%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,27 +1,24 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length][amount + 1];
-        for (int i = 0; i < coins.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return helper(0, coins, amount, dp);
+        int n = coins.length ; 
+        int dp[][] = new int[n+1][amount+1];
+        for(int[] a : dp)Arrays.fill(a,-1);
+        return ways(0,amount,coins,n,dp);
     }
 
-    public int helper(int i, int[] coins, int amount, int[][] dp) {
-        if (i >= coins.length || amount < 0)
-            return 0;
-        if (amount == 0)
-            return 1;
+    public int ways(int i , int amount, int[] coins, int n , int[][]dp){
+        if(amount == 0)return 1 ; 
+        if( i == n )return 0 ; 
 
-        if (dp[i][amount] != -1)
-            return dp[i][amount];
+        if(dp[i][amount] != -1)return dp[i][amount];
 
-        int pick = 0, notpick = 0;
-        if ( amount - coins[i] >= 0) {
-            pick += helper(i, coins, amount - coins[i], dp);
+        int pick = 0 ; 
+        if(amount-coins[i] >= 0){
+            pick = ways(i,amount-coins[i],coins,n,dp);
         }
-        notpick += helper(i + 1, coins, amount, dp);
+        int notpick = ways(i+1,amount,coins,n,dp);
 
-        return dp[i][amount] = pick + notpick;
+        return dp[i][amount] = pick + notpick ; 
+        
     }
 }
