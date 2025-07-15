799af1f1f8864cb9047ddb84ddec03a3472a6ee5
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-07-16 05:18:34 +0530

    Time: 26 ms (61.5%), Space: 65.9 MB (11.11%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
@@ -3,7 +3,8 @@ class Solution {
         Arrays.sort(players);
         Arrays.sort(trainers);
         int res = 0 , i = 0 , j = 0 ; 
-        while(i < players.length && j < trainers.length){
+        int n = players.length , m = trainers.length ; 
+        while(i < n && j < m){
             if(players[i] <= trainers[j]){
                 i++;j++;res++;
             }else j++;
