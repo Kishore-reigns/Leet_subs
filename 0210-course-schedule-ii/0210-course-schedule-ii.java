37cfb1685453de81f46336b16ada4986c6b9740e
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-07-16 04:46:12 +0530

    Time: 5 ms (77.59%), Space: 46.3 MB (30.3%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,40 +1,45 @@
 class Solution {
+    private List<Integer>res ; 
+    private HashSet<Integer> set ; 
+    private boolean[] visited ;
+    private List<List<Integer>> adj ; 
+    private boolean hasCycle = false ; 
+
     public int[] findOrder(int n, int[][] pre) {
-        List<Integer>ans = new ArrayList<>();
-        List<List<Integer>> adj = new ArrayList<>();
-        Queue<Integer>queue  = new LinkedList<>();
+        res = new ArrayList<>();
+        set = new HashSet<>();
+        visited = new boolean[n] ;
+        adj = new ArrayList<>();
 
         for(int i = 0 ; i < n ; i++)adj.add(new ArrayList<>());
+        for(int[] p : pre)adj.get(p[1]).add(p[0]);
 
-        for(int i = 0 ; i < pre.length; i++){
-            adj.get(pre[i][1]).add(pre[i][0]);
-        }
-        int in[] = new int[n] ; 
-        for(int i = 0 ; i < n ; i++){
-            for(int nei : adj.get(i)){
-                in[nei]++ ; 
-            }
+        for(int i = 0 ; i< n ; i++){
+            if(!visited[i])dfs(i,adj);
+            if(hasCycle)return new int[0];
         }
 
-        for(int i = 0 ; i < n; i++){
-            if(in[i] == 0)queue.offer(i);
-        }
+        Collections.reverse(res);
 
-        while(!queue.isEmpty()){
-            int node = queue.poll() ; 
-            ans.add(node);
-            for(int i : adj.get(node)){
-                in[i]-- ; 
-                if(in[i] == 0)queue.offer(i);
-            }
-        }
+        int[] ans = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) ans[i] = res.get(i);
+        return ans;
+    }
 
-        if(ans.size() != n)return new int[0];
-
-        int res[] = new int[ans.size()];
-        for(int i = 0 ; i < ans.size() ; i++)res[i] = ans.get(i) ; 
-        return res; 
-
+    public void dfs(int node , List<List<Integer>> adj){
         
+        if(set.contains(node)){
+            hasCycle = true ; 
+            return ; 
+        }
+        set.add(node);
+
+        for(int neigh : adj.get(node)){
+            if(!visited[neigh])dfs(neigh,adj);
+        }
+
+        set.remove(node);
+        visited[node] = true ; 
+        res.add(node);
     }
 }
