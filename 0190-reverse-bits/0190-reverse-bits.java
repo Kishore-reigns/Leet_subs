0ab7894955537c360d28bd2d821f5fc978c47330
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-07-27 14:37:34 +0530

    Time: 0 ms (100%), Space: 40.9 MB (92.16%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int y = 0 ; 
+        for(int i = 0 ; i < 32 ; i++){
+          y = y << 1 ; 
+            y = y | (n&1) ; 
+            n = n >> 1 ; 
+           
+            
+        }
+        return y ;
+    }
+}
