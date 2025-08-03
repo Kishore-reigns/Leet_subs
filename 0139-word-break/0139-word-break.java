2b37f55b2e2ce816e7d565dfcc41b85e50f96fa3
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-08-03 19:00:57 +0530

    Time: 1 ms (99.46%), Space: 42.1 MB (83.94%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,35 +1,22 @@
 class Solution {
+    Boolean[] dp ; 
     public boolean wordBreak(String s, List<String> wordDict) {
-        Boolean[] dp = new Boolean[s.length() + 1];
-        return check(0,s,wordDict,dp);
+        dp = new Boolean[s.length()];
+
+        return check(0,s,wordDict); 
     }
 
-    public boolean check(int start , String s , List<String> dict , Boolean dp[]){
+    public boolean check(int i , String s , List<String> words){
+        if(i == s.length())return true ; 
+        if(dp[i] != null )return dp[i];
 
-        if(start == s.length())return true ; 
-
-        if(dp[start] != null)return dp[start] ; 
-
-        for(int i = start + 1 ; i <= s.length() ; i++){
-            String prefix = s.substring(start,i);
-            if(dict.contains(prefix)){
-                if(check(i,s,dict,dp))return dp[start] = true ; 
+        for(String w : words){
+            if(i+w.length() <= s.length() && s.substring(i,i+w.length()).equals(w)){
+                if(check(i+w.length(),s,words))return dp[i] = true ; 
             }
         }
-        return dp[start]=  false ; 
+
+        return dp[i] = false ; 
     }
-
-    public boolean helper(int i , String s , List<String> dict){
-
-        if(s.equals(""))return true ; 
-        if( i >= dict.size())return false ; 
-        
-        if(s.contains(dict.get(i))){
-            s = s.replaceFirst(dict.get(i),"");
-            if(helper(i,s,dict))return true ;
-        }
-        
-        return  helper(i+1,s,dict); 
-
-    }
+    
 }
