dc0d306bbf8f605a1614ca640134cf3ed695e133
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-08-20 20:38:37 +0530

    Time: 5 ms (34.02%), Space: 42.1 MB (58.34%) - LeetHub

---
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int res = -1 ; 
+        int n = nums.length ; 
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                if(nums[i] < nums[j]){
+                    res = Math.max(res,nums[j]-nums[i]);
+                }
+            }
+        }
+
+        return res ; 
+    }
+}
