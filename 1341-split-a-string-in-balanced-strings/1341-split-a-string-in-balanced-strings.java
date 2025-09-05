e77c015675e1aef313863abca5eebbef24ef784d
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-09-05 22:12:19 +0530

    Time: 0 ms (100%), Space: 42.1 MB (6.61%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int res = 0 , count = 0 ; 
+        for(char c : s.toCharArray()){
+            if(c == 'R')count++ ;
+            else count-- ; 
+            if(count == 0)res++ ; 
+        }
+
+        return res ; 
+    }
+}
