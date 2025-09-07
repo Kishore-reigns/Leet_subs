c4b0df38e0e32a817389544ae83fda81a887a7e2
Malarvannan M <112459807+Kishore-reigns@users.noreply.github.com>
2025-09-07 17:28:05 +0530

    Time: 0 ms (100%), Space: 41.5 MB (65.81%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxDepth(String s) {
+        int res = 0 ; 
+        int count = 0 ; 
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                count++ ; 
+                res = Math.max(res,count);
+            }else if(c == ')')count-- ; 
+
+        }
+
+        return res ; 
+    }
+}
